4f721b770ef73331e958f7c798d1987dd2fa9898
SalC124 <saltcal124@gmail.com>
2023-12-01 07:38:04 -0500

    Revision 8

--- .vscode/BinaryShenanigans.java
+++ .vscode/BinaryShenanigans.java
@@ -4,12 +4,12 @@ public class BinaryShenanigans {
         try (Scanner input = new Scanner(System.in)) {
             System.out.println("Input a decimal");
             int decimal = input.nextInt();
-            System.out.println("Input the amount of bits");
-            int bits = input.nextInt();
-            System.out.println(DecToBi(decimal,bits));
+            System.out.println(DecToBi(decimal));
         }
     }
-    public static String DecToBi(int decimal, int bits){
+    public static String DecToBi(int decimal){
+        int bits;
+        for(bits = 1; decimal + 1 > Math.pow(2,bits);bits++){}
         String binary = "0".repeat(bits);
         int ogBinaryLength = binary.length();
         for(int i = 1; i <= decimal; i++) {
@@ -22,7 +22,7 @@ public class BinaryShenanigans {
                 }else if(pos == ogBinaryLength - 1 && wasChanged == 0){
                     binary = binary.substring(0,pos) + "1";
                 }
-            }
+            }System.out.println(binary);
         }return binary;
     }
 }
--- .vscode/StringTests.java
+++ .vscode/StringTests.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class StringTests{
     public static void main(String[] Args) {
         try (Scanner input = new Scanner(System.in)){
+            System.out.println("enetr how many thimes you want a to be");
             int N = input.nextInt();
             String newString = "a".repeat(N);
             System.out.println(newString);
+
+            System.out.println("what is the deciaml numebr?");
+            int decimal = input.nextInt();
+
+            int bits;
+            for(bits = 1; decimal + 1 > Math.pow(2,bits);bits++){
+
+            }
+            System.out.println(bits);
+
         }
     }
 }
